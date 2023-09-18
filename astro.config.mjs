d8cefbf2dbddc283ea4634bd9b453ca4ff2e0759
tomasvebr <tomas.vebr@trustly.com>
2023-09-18 15:07:34 +0200

    commit 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://trustlyinternalsupport.github.io/',
-    base: '/ibanvalidation',
+    site: 'https://trustlyinternalsupport.github.io/ibanvalidation',
 });
